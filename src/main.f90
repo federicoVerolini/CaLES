2d5298ede7046ccf1707f169301f035afdeb6af5
Soaringxmc <soaringxmc@DESKTOP-GUCJFM4.localdomain>
2023-11-21 10:39:44 +0100

    0

--- src/main.f90
+++ src/main.f90
@@ -355,6 +355,13 @@ program cans
   call cmpt_bcuvw(cbcvel_wm,n,bcvel,is_bound,.true.,lo,l,dl,zc,visc,kap_log,b_log,hwm,u,v,w,bctau1,bctau2,bcu,bcv,bcw)
   call bounduvw_wm(cbcvel_wm,n,bcu,bcv,bcw,nb,is_bound,.false.,dl,dzc,dzf,u,v,w)
 
+  write(99,*) 'u'
+  write(99,*) u(0:n(1)+1,0:n(2)+1,0:n(3)+1)
+  write(99,*) 'v'
+  write(99,*) v(0:n(1)+1,0:n(2)+1,0:n(3)+1)
+  write(99,*) 'w'
+  write(99,*) w(0:n(1)+1,0:n(2)+1,0:n(3)+1)
+
   !how about update periodic and halo first
   !then compute wall model
   !then update wall bcs
